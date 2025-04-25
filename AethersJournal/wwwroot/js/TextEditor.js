516c385d9d2318ffadb9015363e4f777090bd80e
SJB7788 <83483363+SJB7788@users.noreply.github.com>
2025-04-25 15:17:11 -0700

    add logic to text editor and add chatbox UI

--- AethersJournal/wwwroot/js/TextEditor.js
+++ AethersJournal/wwwroot/js/TextEditor.js
@@ -1,32 +1,81 @@
-// event to handle paste
-window.handlePaste = function (event) {
-  event.preventDefault();
-  const text = (event.clipboardData || window.clipboardData).getData(
-    "text/plain"
-  );
+window.TextEditor = {
+  currSelection: null,
 
-  const splitText = text.split(/\r?\n/);
-  const selection = window.getSelection();
+  saveSelection: function (event) {
+    const sel = window.getSelection();
+    if (sel.rangeCount > 0) {
+      this.currSelection = sel.getRangeAt(0);
+    }
+  },
 
-  if (!selection.rangeCount) return;
+  restoreSelection: function () {
+    const sel = window.getSelection();
 
-  const range = selection.getRangeAt(0);
-  range.deleteContents();
+    if (this.currSelection) {
+      sel.removeAllRanges();
+      sel.addRange(this.currSelection);
+    }
+  },
 
-  splitText.forEach((text) => {
-    let node;
+  handleFormat: function (event) {
+    this.restoreSelection();
+    const format = event.target.getAttribute("data-attribute");
+    const element = this.currSelection.startContainer.parentElement;
+    console.log(element);
 
-    if (text.trim() === "") {
-      node = document.createElement("div");
-      node.appendChild(document.createElement("br"));
+    const span = document.createElement("span");
+
+    if (element.nodeType === 1) {
+      if (
+        (element.tagName.toLowerCase() === "span") &
+        (element.style.fontWeight === "bold")
+      ) {
+        console.log("he");
+        span.textContent = element.textContent;
+      }
     } else {
-      node = document.createElement("div");
-      node.textContent = text;
+      span.style.fontWeight = "bold";
     }
 
-    range.insertNode(node);
+    element.remove();
+    this.currSelection.insertNode(span);
+  },
 
-    range.setStartAfter(node);
-    range.collapse(true);
-  });
+  checkContainer: function (event) {
+    console.log(this.currSelection);
+  },
+
+  handlePaste: function (event) {
+    event.preventDefault();
+    const text = (event.clipboardData || window.clipboardData).getData(
+      "text/plain"
+    );
+
+    const splitText = text.split(/\r?\n/);
+    const selection = window.getSelection();
+
+    if (!selection.rangeCount) return;
+
+    const range = selection.getRangeAt(0);
+    range.deleteContents();
+
+    splitText.forEach((text) => {
+      let node;
+
+      if (text.trim() === "") {
+        node = document.createElement("div");
+        node.appendChild(document.createElement("br"));
+      } else {
+        node = document.createElement("div");
+        node.textContent = text;
+      }
+
+      range.insertNode(node);
+
+      range.setStartAfter(node);
+      range.collapse(true);
+    });
+  },
 };
+
+document.addEventListener("mouseup", () => TextEditor.saveSelection());
