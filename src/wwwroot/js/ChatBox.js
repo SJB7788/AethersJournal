caf2d9181e4b72b6dc6c6bd2fcbbfa1b4d37c542
SJB7788 <83483363+SJB7788@users.noreply.github.com>
2026-03-08 00:18:26 -0800

    fix bug with chat history when switching journals

--- src/wwwroot/js/ChatBox.js
+++ src/wwwroot/js/ChatBox.js
@@ -1,24 +1,32 @@
 window.ChatBox = {
-    openOrCloseChatbox: function (event) {
-        const dataTarget = event.currentTarget.getAttribute("data-target");
-        
-        const chatboxBody = document.querySelector(dataTarget);
-        chatboxBody.classList.toggle("visible");
-    },
-    
-    addMessage: function (chatboxDiv, chatMessage, isHuman) {
-        const messageDiv = document.createElement('div');
-        messageDiv.className = isHuman ? 'user-message' : 'ai-message';
-        messageDiv.textContent = chatMessage;
-        
-        chatboxDiv.appendChild(messageDiv);
-        
-        // Scroll to the bottom to show the new message
-        messageDiv.scrollIntoView({behavior: "smooth"});
+  openOrCloseChatbox: function (event) {
+    const dataTarget = event.currentTarget.getAttribute("data-target");
 
-        // Remove chatboxDiv inner text
-        const input = document.getElementById("chatbox-input");
-        input.value = "";
-        input.focus();
-    }
-}
+    const chatboxBody = document.querySelector(dataTarget);
+    chatboxBody.classList.toggle("visible");
+  },
+
+  addMessage: function (chatboxDiv, chatMessage, isHuman) {
+    const messageDiv = document.createElement("div");
+    messageDiv.className = isHuman ? "user-message" : "ai-message";
+    messageDiv.textContent = chatMessage;
+
+    chatboxDiv.appendChild(messageDiv);
+
+    // Scroll to the bottom to show the new message
+    messageDiv.scrollIntoView({ behavior: "smooth" });
+
+    // Remove chatboxDiv inner text
+    const input = document.getElementById("chatbox-input");
+    input.value = "";
+    input.focus();
+  },
+
+  clearMessage: function (chatboxDiv) {
+    chatboxDiv.innerHTML = "";
+  },
+
+  scrollToBottom: function (element) {
+    element.scrollTop = element.scrollHeight;
+  },
+};
